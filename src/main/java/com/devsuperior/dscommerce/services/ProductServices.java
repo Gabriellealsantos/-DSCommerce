1b06956d519ebb9aa36cf6be0e32f679868d3d56
gabriellealsantos <gabriel.lealsantos@hotmail.com>
2023-05-17 09:57:38 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/services/ProductServices.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductServices.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +22,11 @@ public class ProductServices {
     public ProductDTO findById(Long id) {
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
