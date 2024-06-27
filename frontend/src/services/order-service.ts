b1fdc0a8207cfca2fece79b8fc81bbe2854f7a3b
Gabriel <gabriel.lealsantos@hotmail.com>
2024-06-27 11:41:35 -0300

    feat: Place order

--- frontend/src/services/order-service.ts
+++ frontend/src/services/order-service.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import { OrderDTO } from "../models/order";
 
 export function findByIdRequest(id: number) {
 
@@ -7,6 +8,19 @@ export function findByIdRequest(id: number) {
     const config: AxiosRequestConfig = {
         url: `/orders/${id}`,
         withCredentials: true
+    }
+
+    return requestBackend(config);
+}
+
+
+export function placeOrderResquest(cart: OrderDTO) {
+
+    const config: AxiosRequestConfig = {
+        method: "POST",
+        url: `/orders`,
+        withCredentials: true,
+        data: cart
     }
 
     return requestBackend(config);
