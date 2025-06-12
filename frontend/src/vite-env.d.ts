990e081ba256b78de479e3fe0c8cfd0888ad918e
Gabriellealsantos <gabriel.lealsantos@hotmail.com>
2025-06-12 18:36:38 -0300

    Melhorando tipagem

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string;
+  readonly VITE_CART_KEY: string;
+  readonly VITE_TOKEN_KEY: string;
+  readonly VITE_CLIENT_ID: string;
+  readonly VITE_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
